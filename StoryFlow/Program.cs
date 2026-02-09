cc34462ab705ff6cc42c3f2c0472a20881d2d020
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-02-09 15:30:14 +0100

    feat: create database connection

--- StoryFlow/Program.cs
+++ StoryFlow/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using StoryFlow_Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<MyDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnectionString")));
 
 var app = builder.Build();
 
---
+++ StoryFlow_Database/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoryFlow_Database
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
