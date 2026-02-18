006f9dc0cb3861504ef951a9fba25d03a25166f9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-02-19 00:51:47 +0100

    feat: generate login, register, navbar and profile components

--- StoryFlow_Client/src/app/app.module.ts
+++ StoryFlow_Client/src/app/app.module.ts
@@ -8,11 +8,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { ProfileComponent } from './components/auth/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
