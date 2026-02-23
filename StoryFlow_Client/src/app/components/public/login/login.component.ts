07be0b1ddc234719be57fb1478e96fd2b555743b
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-02-23 20:17:14 +0100

    feat: create login skeleton

--- StoryFlow_Client/src/app/components/public/login/login.component.ts
+++ StoryFlow_Client/src/app/components/public/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  hide = signal(true);
+  form: any = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+  changePasswordVisibility(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  login() {
+    console.log(this.form);
+  }
 }
