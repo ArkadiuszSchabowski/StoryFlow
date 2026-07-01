14fba6596bed6a7947c07d765b4ffaae15e3c392
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-07-01 19:43:56 +0200

    fix: update CORS

--- StoryFlow/Program.cs
+++ StoryFlow/Program.cs
@@ -72,7 +72,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("StoryFlowPolicy", policy =>
     {
         policy.WithOrigins("http://localhost:4200",
-            "https://red-hill-0235bd503.7.azurestaticapps.net/login")
+            "https://red-hill-0235bd503.7.azurestaticapps.net")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
