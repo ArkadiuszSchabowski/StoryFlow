7da826e3ee0e3e316cb6648bf8b6b0f038a6f005
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-07-01 20:05:15 +0200

    fix: uncomment use middleware

--- StoryFlow/Program.cs
+++ StoryFlow/Program.cs
@@ -104,7 +104,7 @@ builder.Services.AddAuthentication(option =>
 
 var app = builder.Build();
 
-//app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.UseCors("StoryFlowPolicy");
 
