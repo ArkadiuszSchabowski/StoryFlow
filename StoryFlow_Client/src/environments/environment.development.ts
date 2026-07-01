a823acfa5bf53d5df80999e9e4835547c143cde8
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-07-01 20:04:09 +0200

    fix: update api url

--- StoryFlow_Client/src/environments/environment.development.ts
+++ StoryFlow_Client/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     docker: false,
-    apiUrl: 'https://red-hill-0235bd503.7.azurestaticapps.net/api/'
+    apiUrl: 'https://storyflow-b4ahcdfcf7fzcmeq.polandcentral-01.azurewebsites.net/api/'
 }
 
 // http://localhost:5000/api/
