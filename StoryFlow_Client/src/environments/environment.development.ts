d8688b0fd423143d6b54ca3014a8103255730342
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-07-05 23:55:46 +0200

    chore: change api url for production

--- StoryFlow_Client/src/environments/environment.development.ts
+++ StoryFlow_Client/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     docker: false,
-    apiUrl: 'http://localhost:5000/api/'
+    apiUrl: 'https://storyflow-b4ahcdfcf7fzcmeq.polandcentral-01.azurewebsites.net/api/'
 }
 
+//http://localhost:5000/api/
 //
-//https://storyflow-b4ahcdfcf7fzcmeq.polandcentral-01.azurewebsites.net/api/
