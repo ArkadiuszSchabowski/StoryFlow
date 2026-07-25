2ca93d2d70434eef14730e035ffe0645542f7532
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-07-25 02:39:13 +0200

    test: comment prefetch loader assets

--- StoryFlow_Client/src/app/components/public/home/home.component.ts
+++ StoryFlow_Client/src/app/components/public/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.prefetchLoaderAssets();
+    // this.prefetchLoaderAssets();
   }
 
   private prefetchLoaderAssets(): void {
