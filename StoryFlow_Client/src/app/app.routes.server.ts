700715af4f4964fcddfb43f1e8c5311d2d3713fe
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-07-25 21:47:45 +0200

    fix: update app routes

--- StoryFlow_Client/src/app/app.routes.server.ts
+++ StoryFlow_Client/src/app/app.routes.server.ts
@@ -2,7 +2,51 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'sezon/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'text/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'register',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'error-page',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'sezon-selection',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'profile',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'create-story',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'create-quiz',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'library',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Client,
+  },
 ];
