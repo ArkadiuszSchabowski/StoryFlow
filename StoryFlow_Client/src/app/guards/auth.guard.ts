648371ae90b650bd7f3547cd19e4cd9bad7e6457
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2026-08-09 19:11:17 +0200

    fix: remove unnecessary toastr

--- StoryFlow_Client/src/app/guards/auth.guard.ts
+++ StoryFlow_Client/src/app/guards/auth.guard.ts
@@ -5,14 +5,12 @@ import { AuthService } from '../_services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  const toastr = inject(ToastrService);
   const router = inject(Router);
 
   const token = authService.currentUserSource.getValue();
   if (token) {
     return true;
   } else {
-    toastr.error('Zaloguj się, by przejść na tę stronę.');
     router.navigateByUrl('/login');
     return false;
   }
